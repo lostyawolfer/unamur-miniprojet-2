a6c577e83509b48c977450508c8984b308a622f2
lulubeirlu <lucas.bardonnaux@gmail.com>
2025-11-14 12:04:56 +0100

    Function to do 1

---
+++ console_gr_27.py
@@ -0,0 +1,22 @@
+"""
+Choix:
+------
+Faire avec une liste de liste. 5X5
+
+Choisis la direction avec l'inclinaison. A pour déplacer, B pour Lacher
+
+
+"""
+
+def create_new_pieces() -> bool:
+    ...
+
+def collide_with_others():
+    ...
+
+board = [[0]*5 for i in range(5)]
+
+def execute_order():
+    ...
+
+
--- gamepad_gr_27.py
+++ gamepad_gr_27.py
@@ -1,1 +1,18 @@
+"""
+Ce micro bit doit juste afficher
+"""
+
+
+board = [[0]*5 for i in range(5)]
+
+
+def show_view_board():
+    ...
+
+def a_is_pressed_gamepad():
+    ...
+
+def b_is_pressed_gamepad():
+    ...
+
 
