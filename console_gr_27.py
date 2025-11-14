df83c7da0733e8172e57b5fa1b8e5657bc1deb42
lostyawolfer <lostyawolfer@gmail.com>
2025-11-14 12:36:03 +0100

    add list[list[int]] to microbit.Image convertor

--- console_gr_27.py
+++ console_gr_27.py
@@ -1,10 +1,12 @@
+import microbit
+# import radio
 """
 Choix:
 ------
 Faire avec une liste de liste. 5X5
 
 Choisis la direction avec l'inclinaison. A pour déplacer, B pour Lacher
-
+"""
 
 
 
@@ -15,6 +17,17 @@ def collide_with_others():
     ...
 
 board = [[0]*5 for i in range(5)]
+def get_board_image(board: list) -> microbit.Image:
+    '''Create a microbit image object from the list[list[int]] format'''
+    res = ''
+    for i in board:
+        for j in i:
+            res += str(j)
+        res += ':'
+    return microbit.Image(res)
+def get_board_list(board: microbit.Image) -> list:
+    ...
+
 
 def execute_order():
     ...
